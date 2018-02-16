fc75a910b67eb56f207018be3aa406d40d330a21
thufir <thufir@dur.bounceme.net>
2018-02-15 23:36:55 -0800

    init

---
+++ src/main/kotlin/net/bounceme/dur/kotlin/soap/app.kt
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dur.bounceme.net.kotlin.soap
+
+/*
+
+  @author thufir
+  Created on 15-Feb-2018
+*/
+
+class App {
+
+}
