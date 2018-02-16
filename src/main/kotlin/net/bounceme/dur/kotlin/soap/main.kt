732f300fb4bde892b04ecc3d74a8a0793fbeeccd
thufir <thufir@dur.bounceme.net>
2018-02-16 00:34:10 -0800

    increment

--- src/main/kotlin/net/bounceme/dur/kotlin/soap/main.kt
+++ src/main/kotlin/net/bounceme/dur/kotlin/soap/main.kt
@@ -1,16 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package net.bounceme.dur.kotlin.soap
-
-/*
-
-  @author thufir
-  Created on 15-Feb-2018
-*/
-
 
 fun main(args: Array<String>) {
     println("Hello gradle script Kotlin world!")
